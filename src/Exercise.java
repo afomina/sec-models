22c70a4a5cd62d0d1b4eb1338c182a5fec607d7f
Alexandra <aleksa.fom@gmail.com>
2015-11-11 07:42:54 +0300

    exercise intro

---
+++ src/Exercise.java
@@ -0,0 +1,8 @@
+/**
+ * Created by alexa on 08.11.2015.
+ */
+public class Exercise {
+    public void doEx(Subject admin, Subject user, SecurityObject adminFolder, SecurityObject userFolder) {
+
+    }
+}
